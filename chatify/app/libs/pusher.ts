923c0ab6a582e681173e04b4a3e70eb879ae60c5
Shubham Mishra <sahilmishra2016@gmail.com>
2024-09-01 22:55:34 +0530

    pusher

--- chatify/app/libs/pusher.ts
+++ chatify/app/libs/pusher.ts
@@ -3,18 +3,15 @@ import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  key: "0233f81bbd83e980270e",
   secret: process.env.PUSHER_SECRET!,
   cluster: "ap2",
   useTLS: true,
 });
-export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  {
-    channelAuthorization: {
-      endpoint: "/api/pusher/auth",
-      transport: "ajax",
-    },
-    cluster: "ap2",
-  }
-);
+export const pusherClient = new PusherClient("0233f81bbd83e980270e", {
+  channelAuthorization: {
+    endpoint: "/api/pusher/auth",
+    transport: "ajax",
+  },
+  cluster: "ap2",
+});
