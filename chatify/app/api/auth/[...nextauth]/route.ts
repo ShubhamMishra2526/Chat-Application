be686894ef8cdf3f3ea4d6e233776c790178411e
Shubham Mishra <sahilmishra2016@gmail.com>
2024-09-01 23:26:39 +0530

    Update route.ts

--- chatify/app/api/auth/[...nextauth]/route.ts
+++ chatify/app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "@/app/libs/prismadb";
 
-const authOptions: AuthOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
